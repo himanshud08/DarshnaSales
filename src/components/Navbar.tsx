eb3d9f79480affcfd0e9fbc64e560e7d3a5ddb0b
himanshud08 <himanshudhoot111@gmail.com>
2025-06-26 19:29:34 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
-import Link from "next/link";
 import Image from "next/image";
 
 const Navbar: React.FC = () => {
